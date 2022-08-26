8073a153632c6311bd841f9b0ff00ba7c1726418
GeronimoNeto <102226234+GeronimoNeto@users.noreply.github.com>
2022-08-26 15:51:02 -0300

    Atualizar registros

--- script.js
+++ script.js
@@ -7,6 +7,18 @@ function removeProduct(id){
     })
     localStorage.setItem("products",JSON.stringify(newProduct))
 }
+function updateProduct(product){
+    const storage = localStorage.getItem("products")
+    const products = storage ? JSON.parse(storage) : []
+    const uProduct = {
+        id:product.id,
+        name:product.name,
+        price:product.price,
+        desc:product.desc
+    }
+    localStorage.setItem("products", JSON.stringify([...products,uProduct]))
+    document.getElementById("update").style.display="none"
+}
 
 function renderProducts(){
     const storage = localStorage.getItem("products")
@@ -21,18 +33,47 @@ function renderProducts(){
             <td class="table-row">${product.price}</td>
             <td class="table-row">${product.desc}</td>
             <td class="table-row">
-                <button class="btn danger" type="button" id="${product.id}">X</button>
+                <button class="btn danger" type="button" id="D${product.id}">X</button>
+                <button class="btn warning" type="button" id="E${product.id}">E</button>
             </td>
         `
         tbody.appendChild(tr)
-        document.getElementById(product.id).addEventListener("click",()=>{
+        document.getElementById(`D${product.id}`).addEventListener("click",()=>{
             removeProduct(product.id)
             renderProducts()
+        })
+        document.getElementById(`E${product.id}`).addEventListener("click",()=>{
+            const update = document.getElementById("update")
+            update.style.display="flex"
+            const eName = document.getElementById("eName")
+            const ePrice = document.getElementById("ePrice")
+            const eDesc = document.getElementById("eDesc")
+
+            eName.value=product.name
+            ePrice.value=product.price
+            eDesc.value=product.desc
+
+            document.querySelector("#update form").dataset.id=product.id
+            
         })
     })
 }
 
-document.addEventListener("DOMContentLoaded", renderProducts)
+function setTheme(){
+    const root = document.querySelector(":root").style
+    const storage = localStorage.getItem("theme")
+    const styles = storage ? JSON.parse(storage) : []
+    styles.forEach((style)=>{
+        root.setProperty("--first", style.first);
+        root.setProperty("--second", style.second);
+        root.setProperty("--text", style.text);
+    })
+}
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    setTheme()
+    renderProducts()
+})
 
 document.getElementById("product-form").addEventListener("submit",(event)=>{
     event.preventDefault()
@@ -51,4 +92,52 @@ document.getElementById("product-form").addEventListener("submit",(event)=>{
     name.value = price.value = desc.value = ""
     renderProducts()
     window.scrollTo({top:document.body.scrollHeight,behavior:'smooth'})
+})
+
+document.querySelector("#update form").addEventListener("submit",(event)=>{
+    event.preventDefault()
+    const product = {
+        id:document.querySelector("#update form").dataset.id,
+        name:document.getElementById("eName").value,
+        price:document.getElementById("ePrice").value,
+        desc:document.getElementById("eDesc").value
+    }
+    removeProduct(product.id)
+    updateProduct(product)
+    renderProducts()
+})
+document.querySelector("#update form button:last-child").addEventListener("click",(event)=>{
+    event.preventDefault()
+    const update = document.getElementById("update")
+    update.style.display="none"
+})
+
+
+document.getElementById("theme").addEventListener('click',()=>{
+    const themes = [
+        {
+            name: "dark",
+            first: "#333",
+            second: "#1d1d1d",
+            text: "#f8f8f8"
+        },
+        {
+            name: "light",
+            first: "white",
+            second: "#f8f8f8",
+            text: "#333"
+        }
+    ]
+    const storage = localStorage.getItem("theme")
+    const theme = storage ? JSON.parse(storage) : []
+    if(theme.length==0){
+        localStorage.setItem("theme",JSON.stringify([themes[1]]))
+    }else if(theme[0]?.name=="light"){
+        localStorage.setItem("theme",JSON.stringify([themes[0]]))
+    }else if(theme[0]?.name=="dark"){
+        localStorage.setItem("theme",JSON.stringify([themes[1]]))
+    }
+    
+
+    setTheme()
 })
